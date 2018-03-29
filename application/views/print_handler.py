315fb6b23ffe4ad1acb9381e53f7f2d6f7d10998
GeevesH-IS <hugo.geeves@informed.com>
2018-03-29 09:50:12 +0100

    print_handler.py: add order_code to template variables
    As specified in ticket [CCN3-750] and subticket [CCN3-751]

--- application/views/print_handler.py
+++ application/views/print_handler.py
@@ -15,7 +15,9 @@ def base_print_handler(request, page):
 
     if request.method == 'GET':
         application_id_local = request.GET["id"]
+        order_code = request.GET["orderCode"]
         variables = {'app_id': application_id_local,
+                     'order_code': order_code,
                      'print_class': 'visually-hidden'}
         if page == 'master-summary':
             variables.update(declaration_summary(request, print=True))
