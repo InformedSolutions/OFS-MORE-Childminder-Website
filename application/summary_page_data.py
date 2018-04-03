cbdd2521289a1a36a4940b98c4e2d9e3494d24e7
James Cruddas <james.cruddas@ros.gov.uk>
2018-04-03 20:35:33 +0100

    summary_page_data.py: Correct text for overnight care

--- application/summary_page_data.py
+++ application/summary_page_data.py
@@ -3,7 +3,7 @@ import collections
 
 
 childcare_type_name_dict = collections.OrderedDict([('childcare_age_groups', 'What age groups will you be caring for?'),
-                                                   ('overnight_care', 'Are you providing overnight care?')])
+                                                   ('overnight_care', 'Will you be looking after children overnight?')])
 
 
 childcare_type_link_dict = collections.OrderedDict([('childcare_age_groups', 'Type-Of-Childcare-Age-Groups-View'),
