aee2ab9bb6a68d8e5c95ca4ae16521915ca5b85c
James Cruddas <james.cruddas@ros.gov.uk>
2018-04-12 20:18:25 +0100

    selenium: Switch host address

--- application/tests/test_selenium.py
+++ application/tests/test_selenium.py
@@ -24,7 +24,7 @@ selenium_task_executor = None
 @override_settings(ALLOWED_HOSTS=['*'])
 class ApplyAsAChildminder(LiveServerTestCase):
     port = 8000
-    host = '127.0.0.1'
+    host = '0.0.0.0'
     current_year = datetime.now().year
 
     def setUp(self):
