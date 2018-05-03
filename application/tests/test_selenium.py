439548e5c4b41e7fba03ee67017690ec89894403
thomas-95is <thomas.stokes@informed.com>
2018-05-03 10:00:12 +0100

    test_selenium.py: Fix test_invalid_dbs_security_question_raises_error

--- application/tests/test_selenium.py
+++ application/tests/test_selenium.py
@@ -984,10 +984,10 @@ class ApplyAsAChildminder(LiveServerTestCase):
         # Test no DBS number, too short a DBS number, too long a DBS number and an incorrect DBS number.
         test_numbers = ['', '1', '0123456789101112', '987654321987']
         for test_number in test_numbers:
+            selenium_task_executor.get_driver().find_element_by_name("security_answer").clear()
             selenium_task_executor.get_driver().find_element_by_name("security_answer").send_keys(test_number)
             selenium_task_executor.get_driver().find_element_by_xpath("//input[@value='Save and continue']").click()
             self.assertIn("problem", selenium_task_executor.get_driver().find_element_by_class_name("error-summary").text)
-            selenium_task_executor.get_driver().find_element_by_id('back').click()  # Go back to remove errors from previous test_number.
 
     @try_except_method
     def test_save_and_continue_childcare_address_redirects_to_manual_entry(self):
