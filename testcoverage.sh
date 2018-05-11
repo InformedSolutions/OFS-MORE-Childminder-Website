ff18b6549df1a94a495852b4cb73bc7fe790938d
Christopher Josephs <christopher.josephs@informed.com>
2018-05-11 16:46:47 +0100

    Don't display error if htmlcov directory is attempted to be removed but does not exist yet

--- testcoverage.sh
+++ testcoverage.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-rm -r htmlcov
+rm -r htmlcov 2> /dev/null
 coverage erase
 coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-childminder/manage.py test --settings=childminder.settings.dev --exclude-tag=selenium --no-input
 coverage report -m --omit=.env/*,*/migrations/*,*/__init__.py,*/tests.py,*/models.py,manage.py,/home/vagrant/example-venv/*
