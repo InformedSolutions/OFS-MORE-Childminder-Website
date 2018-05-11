18129001cf34463e82df41930b782d91a848598e
Christopher Josephs <christopher.josephs@informed.com>
2018-05-11 18:02:25 +0100

    Omit coverage not related to app-childminder

--- testcoverage.sh
+++ testcoverage.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Script to test unit test coverage for app-childminder and produce html document from report
 rm -r htmlcov 2> /dev/null
 coverage erase
 coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-childminder/manage.py test --settings=childminder.settings.dev --exclude-tag=selenium --no-input
