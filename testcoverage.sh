bf5bfb5b70e54a0d4da21565e28a7a3dd269335f
Christopher Josephs <christopher.josephs@informed.com>
2018-05-11 17:40:08 +0100

    Omit coverage not related to app-childminder

--- testcoverage.sh
+++ testcoverage.sh
@@ -2,6 +2,6 @@
 rm -r htmlcov 2> /dev/null
 coverage erase
 coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-childminder/manage.py test --settings=childminder.settings.dev --exclude-tag=selenium --no-input
-coverage report -m --omit=.env/*,*/migrations/*,*/__init__.py,*/tests.py,*/models.py,manage.py,/home/vagrant/example-venv/*
-coverage html -d
+coverage report -m --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*
+coverage html --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* -d htmlcov
 firefox htmlcov/index.html
