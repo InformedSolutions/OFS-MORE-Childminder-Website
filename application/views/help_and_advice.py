4daa080140070ab91683e8622b8d4aa1fd83aadc
Alice King <alice.king@informed.com>
2018-05-17 09:03:44 +0100

    handle an unauthenticated user

--- application/views/help_and_advice.py
+++ application/views/help_and_advice.py
@@ -13,22 +13,23 @@ def help_and_advice(request):
     :return: an HttpResponse object with the rendered Help and Advice template
     """
     application_id_local = request.GET.get('id')
-    application = Application.objects.get(pk=application_id_local)
-    url_params = {'id': application_id_local, 'get': True}
-
-    # render either confirmation or task list view, depending on if application has been submitted
-    if application.application_status == 'SUBMITTED':
-        return_view = 'Payment-Confirmation'
-        url_params['orderCode'] = str(application.order_code)
-    else:
-        return_view = 'Task-List-View'
-
-    # build url to be passed to the return button
-    return_url = build_url(return_view, get=url_params)
 
     context = {
-        'id': application_id_local,
-        'return_url': return_url
+        'id': application_id_local
     }
+
+    if application_id_local is not None:
+        application = Application.objects.get(pk=application_id_local)
+        url_params = {'id': application_id_local, 'get': True}
+
+        # render either confirmation or task list view, depending on if application has been submitted
+        if application.application_status == 'SUBMITTED':
+            return_view = 'Payment-Confirmation'
+            url_params['orderCode'] = str(application.order_code)
+        else:
+            return_view = 'Task-List-View'
+
+        # build url to be passed to the return button
+        context['return_url'] = build_url(return_view, get=url_params)
 
     return render(request, 'help-and-advice.html', context)
