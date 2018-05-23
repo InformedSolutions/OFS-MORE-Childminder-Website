e247b0d1f25195044a6188a54f8889bf785b6c71
andrewskeaIS <andrew.skea@informed.com>
2018-05-23 10:24:21 +0100

    Bug fix for incorrect additional phone

--- application/views/contact_info.py
+++ application/views/contact_info.py
@@ -33,6 +33,11 @@ def contact_phone(request):
         if len(acc.mobile_number) == 0 and 'f' not in request.GET:
             flag = '&f=1'
             return HttpResponseRedirect(reverse('Contact-Phone-View') + '?id=' + app_id + flag)
+
+        if application.application_status == 'FURTHER_INFORMATION':
+            mobile_form.error_summary_template_name = 'returned-error-summary.html'
+            mobile_form.error_summary_title = 'There was a problem on this page'
+
         variables = {
             'mobile_form': mobile_form,
             'add_phone_form': add_phone_form,
@@ -77,16 +82,19 @@ def contact_phone(request):
 
                 return HttpResponseRedirect(reverse('Contact-Summary-View') + '?id=' + app_id + flag)
 
-        else:
-            variables = {
-                'mobile_form': mobile_form,
-                'add_phone_form': add_phone_form,
-                'application_id': app_id,
-                'login_details_status': application.login_details_status,
-                'childcare_type_status': application.childcare_type_status
-            }
+        if application.application_status == 'FURTHER_INFORMATION':
+            mobile_form.error_summary_template_name = 'returned-error-summary.html'
+            mobile_form.error_summary_title = 'There was a problem on this page'
 
-            return render(request, 'contact-phone.html', variables)
+        variables = {
+            'mobile_form': mobile_form,
+            'add_phone_form': add_phone_form,
+            'application_id': app_id,
+            'login_details_status': application.login_details_status,
+            'childcare_type_status': application.childcare_type_status
+        }
+
+        return render(request, 'contact-phone.html', variables)
 
 
 def contact_summary(request):
