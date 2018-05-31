3a3cdefd328a5876ffd7c6f70123d30d29dc4a01
Rica Duchateau <rica.duchateau@informed.com>
2018-05-31 15:39:14 +0100

    Set Confirm button link on summary page to task list when task completed

--- application/views/childcare_type.py
+++ application/views/childcare_type.py
@@ -288,7 +288,8 @@ def childcare_type_summary(request):
 
         variables = submit_link_setter(variables, table_list, 'personal_details', app_id)
 
-        variables['submit_link'] = reverse('Personal-Details-Name-View')
+        if application.childcare_type_status != 'COMPLETED':
+            variables['submit_link'] = reverse('Personal-Details-Name-View')
 
         return render(request, 'generic-summary-template.html', variables)
 
