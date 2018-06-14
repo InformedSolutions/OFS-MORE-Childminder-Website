96cad96f5ac545d770bfc6b0eb3a6fe0974f7b2c
Alice King <alice.king@informed.com>
2018-06-14 14:07:00 +0100

    handle empty reference table

--- application/application_reference_generator.py
+++ application/application_reference_generator.py
@@ -17,20 +17,29 @@ def create_application_reference():
     try:
         last_allocated_reference_details_for_application_type = ApplicationReference.objects.all().first()
 
-        # Roll integer field by 1
-        candidate_reference = last_allocated_reference_details_for_application_type.reference + 1
+        if last_allocated_reference_details_for_application_type:
+            # Roll integer field by 1
+            candidate_reference = last_allocated_reference_details_for_application_type.reference + 1
 
-        # Assert reference number has not previously been assigned
-        reference_previously_allocated = Application.objects.filter(application_reference=candidate_reference).exists()
+            # Assert reference number has not previously been assigned
+            reference_previously_allocated = Application.objects.filter(application_reference=candidate_reference).exists()
 
-        if not reference_previously_allocated:
-            # If not previously allocated, persist new latest assigned value and yield to caller
-            last_allocated_reference_details_for_application_type.reference = candidate_reference
-            last_allocated_reference_details_for_application_type.save()
+            if not reference_previously_allocated:
+                # If not previously allocated, persist new latest assigned value and yield to caller
+                last_allocated_reference_details_for_application_type.reference = candidate_reference
+                last_allocated_reference_details_for_application_type.save()
+                return settings.APPLICATION_PREFIX + str(candidate_reference)
+
+            # In event application reference has already been allocated, recurse to attempt yield of next incremented value
+            return create_application_reference()
+
+        else:
+            candidate_reference = 1000001
+            ApplicationReference.objects.create(
+                reference=1000001
+            )
             return settings.APPLICATION_PREFIX + str(candidate_reference)
 
-        # In event application reference has already been allocated, recurse to attempt yield of next incremented value
-        return create_application_reference()
     except Exception as e:
         logger.error('Failed to allocate application reference number: ' + str(e))
 
