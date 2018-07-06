0228e7f7f8084238d5f13d64426592bfe84cd124
Alice King <alice.king@informed.com>
2018-07-06 09:57:32 +0100

    add expiry page to authentication exempt url list

--- childminder/settings/dev.py
+++ childminder/settings/dev.py
@@ -41,6 +41,7 @@ AUTHENTICATION_EXEMPT_URLS = (
     r'^' + URL_PREFIX + '/code-resent/.*$',
     r'^' + URL_PREFIX + '/security-code/.*$',
     r'^' + URL_PREFIX + '/link-used/$',
+    r'^' + URL_PREFIX + '/link-expired/$',
     r'^' + URL_PREFIX + '/new-code/.*$',
     r'^' + URL_PREFIX + '/djga/+',
     r'^' + URL_PREFIX + '/sign-in/',
