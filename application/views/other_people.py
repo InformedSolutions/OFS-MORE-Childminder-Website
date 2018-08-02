5895f3960728b47298f5fa7a210e9fcf5c0b2375
Rica Duchateau <rica.duchateau@informed.com>
2018-08-02 11:23:45 +0100

    Remove setting flagged health check resend limit to zero when flagged as handled in ARC

--- application/views/other_people.py
+++ application/views/other_people.py
@@ -718,11 +718,6 @@ def other_people_summary(request):
             if adult.health_check_status != 'Done':
                 adult_health_check_status_list.append('To do')
 
-            # If the adult health check status is marked as flagged, set the email resend limit to 0
-            if adult.health_check_status == 'Flagged':
-                adult.email_resent = 0
-                adult.save()
-
             # Counter for table object to correctly set link in generic-error-summary template for flagged health check.
             table = Table([adult.pk])
             table.loop_counter = index + 1
