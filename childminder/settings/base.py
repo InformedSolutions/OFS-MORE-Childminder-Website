8d86b16e3388c31d858cdf2c20772d35854e8ba6
James Cruddas <james.cruddas@informed.com>
2018-08-08 09:08:50 +0100

    Remove datetime import from settings

--- childminder/settings/base.py
+++ childminder/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-from datetime import datetime
 
 # Server name for showing server that responded to request under load balancing conditions
 SERVER_LABEL = os.environ.get('SERVER_LABEL')
