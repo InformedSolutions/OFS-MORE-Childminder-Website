9a120e404f4e3a35a2c527814a3a068b675f02a0
James Cruddas <james.cruddas@informed.com>
2018-09-17 16:31:24 +0100

    Add test class

---
+++ application/tests/tests_application/test_your_children.py
@@ -0,0 +1,37 @@
+"""
+Tests targetting the payment process
+"""
+
+from unittest import mock
+
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+from .base import ApplicationTestBase
+from ...payment_service import *
+from ...models import Payment, Application
+
+
+class YourChildrenTests(TestCase, ApplicationTestBase):
+
+    def setUp(self):
+        with mock.patch('application.notify.send_email') as notify_mock, \
+            mock.patch('application.utils.test_notify_connection') as notify_connection_test_mock:
+
+            notify_connection_test_mock.return_value.status_code = 201
+            notify_mock.return_value.status_code = 201
+
+            # Initial steps required here as client object needs to be authenticated to
+            # access payment pages
+            self.TestAppInit()
+
+            self.TestAppEmail()
+            self.TestValidateEmail()
+            self.TestAppPhone()
+            self.TestContactSummaryView()
+            self.TestTypeOfChildcareAgeGroups()
+            self.TestTypeOfChildcareOvernightCare()
+            self.TestSecurityQuestion()
+            self.AppTestTypeOfChildcareRegister()
+
+            self.TestAppPersonalDetailsNames()
