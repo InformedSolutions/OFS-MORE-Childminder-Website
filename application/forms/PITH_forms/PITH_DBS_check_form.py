5d35affb27a57fdc3cb435818d57505dc44a128a
LRydeard <luke.rydeard@informed.com>
2018-09-18 19:13:58 +0100

    Added additional conditionally_revealed field

--- application/forms/PITH_forms/PITH_DBS_check_form.py
+++ application/forms/PITH_forms/PITH_DBS_check_form.py
@@ -28,11 +28,13 @@ class PITHDBSCheckForm(ChildminderForms):
         self.capita_field_name = self.capita_field + str(self.adult.pk)
         self.dbs_field_name = self.dbs_field + str(self.adult.pk)
         self.on_update_field_name = self.on_update_field + str(self.adult.pk)
+        self.dbs_field_no_update_name = self.dbs_field +'_no_update'+str(self.adult.pk)
 
         self.base_fields = {
             self.dbs_field_name: self.get_dbs_field_data(),
             self.on_update_field_name: self.get_on_update_field_data(),
-            self.capita_field_name: self.get_capita_field_data()
+            self.capita_field_name: self.get_capita_field_data(),
+            self.dbs_field_no_update_name: self.get_dbs_field_data()
         }
 
         self.reveal_conditionally = self.get_reveal_conditionally()
@@ -69,7 +71,7 @@ class PITHDBSCheckForm(ChildminderForms):
         return forms.ChoiceField(
             label='Are they on the DBS update service?',
             choices=self.get_options(),
-            widget=InlineRadioSelect,
+            widget=ConditionalPostInlineRadioSelect,
             required=True,
             error_messages={'required': 'Please say if this person is on the DBS update service'})
 
@@ -84,8 +86,11 @@ class PITHDBSCheckForm(ChildminderForms):
         cleaned_capita_field = self.cleaned_data[self.capita_field_name] == 'True'\
             if self.cleaned_data.get(self.capita_field_name)\
             else None
-        cleaned_dbs_field = self.cleaned_data[self.dbs_field_name]\
-            if self.cleaned_data[self.dbs_field_name] != ""\
+        cleaned_dbs_field = self.data[self.dbs_field_name]\
+            if self.data[self.dbs_field_name] != ""\
+            else None
+        cleaned_dbs_field_no_update = self.data[self.dbs_field_no_update_name] \
+            if self.data[self.dbs_field_no_update_name] != "" \
             else None
         cleaned_on_update_field = self.cleaned_data[self.on_update_field_name] == 'True'\
             if self.cleaned_data[self.on_update_field_name] != ""\
@@ -94,29 +99,39 @@ class PITHDBSCheckForm(ChildminderForms):
 
         if cleaned_capita_field is not None:
             if cleaned_capita_field:
-                if cleaned_dbs_field is None:
-                    self.add_error(self.dbs_field_name, 'Please say if this person has an Ofsted DBS check')
-                elif len(str(cleaned_dbs_field)) != 12:
-                    self.add_error(self.dbs_field_name, 'Check the certificate: the number should be 12 digits long')
-                elif childminder_dbs_duplicates_household_member_check(application, cleaned_dbs_field):
-                    self.add_error(self.dbs_field_name, 'Please enter a different DBS number. '
-                                                        'You entered this number for someone in your childcare location')
-                else:
-                    #TODO Move this code to more appropriate place (e.g form_valid)
-                    update_adult_in_home(self.adult.pk, 'capita', cleaned_capita_field)
-                    update_adult_in_home(self.adult.pk, 'on_update', None)
-                    update_adult_in_home(self.adult.pk, 'dbs_certificate_number', cleaned_dbs_field)
-
-            elif not cleaned_capita_field:
+                self.clean_dbs(cleaned_dbs_field, self.dbs_field_name, application, cleaned_capita_field, cleaned_on_update_field)
+            else:
                 if cleaned_on_update_field is None:
                     self.add_error(self.on_update_field_name, 'Please say if this person is on the DBS update service')
+                elif cleaned_on_update_field:
+                    self.clean_dbs(cleaned_dbs_field_no_update, self.dbs_field_no_update_name, application, cleaned_capita_field, cleaned_on_update_field)
                 else:
-                    update_adult_in_home(self.adult.pk, 'capita', cleaned_capita_field)
-                    update_adult_in_home(self.adult.pk, 'on_update', cleaned_on_update_field)
-                    update_adult_in_home(self.adult.pk, 'dbs_certificate_number', '')
+                    self.update_adult_in_home_fields(cleaned_capita_field, cleaned_on_update_field, '')
 
         return self.cleaned_data
 
     def get_reveal_conditionally(self):
-        return {self.capita_field_name: {True: self.dbs_field_name,
+        return {self.on_update_field_name: {True: self.dbs_field_no_update_name},
+                self.capita_field_name: {True: self.dbs_field_name,
                                          False: self.on_update_field_name}}
+
+    def clean_dbs(self, cleaned_dbs_value, field_name, application, cleaned_capita_value, cleaned_on_update_value):
+        if cleaned_dbs_value is None:
+            self.add_error(field_name, 'Please enter their DBS certificate number')
+        elif len(str(cleaned_dbs_value)) != 12:
+            self.add_error(field_name,
+                           'Check the certificate: the number should be 12 digits long')
+        elif childminder_dbs_duplicates_household_member_check(application, cleaned_dbs_value):
+            self.add_error(field_name, 'Please enter a different DBS number. '
+                                       'You entered this number for someone in your childcare location')
+        else:
+            # TODO Move this code to more appropriate place (e.g form_valid)
+            if '_no_update' in field_name:
+                self.update_adult_in_home_fields(cleaned_capita_value, cleaned_on_update_value, cleaned_dbs_value)
+            else:
+                self.update_adult_in_home_fields(cleaned_capita_value, None, cleaned_dbs_value)
+
+    def update_adult_in_home_fields(self, capita_value, on_update_value, dbs_value):
+        update_adult_in_home(self.adult.pk, 'capita', capita_value)
+        update_adult_in_home(self.adult.pk, 'on_update', on_update_value)
+        update_adult_in_home(self.adult.pk, 'dbs_certificate_number', dbs_value)
