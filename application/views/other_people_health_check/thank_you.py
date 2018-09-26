9a36253ce0308b7606acfdc2fb79619c637406b4
proctorj <james.proctor@informed.com>
2018-09-26 16:46:43 +0100

    Added email checks to test with templates

--- application/views/other_people_health_check/thank_you.py
+++ application/views/other_people_health_check/thank_you.py
@@ -39,84 +39,84 @@ class ThankYou(BaseTemplateView):
         except:
             firstname = 'Applicant'
 
-        # if adult_record.health_check_status == 'To do' or adult_record.health_check_status == 'Started':
-        template_id = '8f5713f5-4437-479e-9fcc-262d0306f58c'
-        email = user_details.email
-        link = str(settings.PUBLIC_APPLICATION_URL) + '/validate/' + create_account_magic_link(user_details)
-        personalisation = {"link": link,
-                           "firstName": firstname,
-                           "Household Member Name": adult_name}
-        r = send_email(email, personalisation, template_id)
-        print(link)
-        # Delete ARC comment if it exists after recompleting the household member health check
-        if ArcComments.objects.filter(table_pk=application_id, field_name='health_check_status').count() > 0:
-            arc_comment = ArcComments.objects.get(table_pk=application_id, field_name='health_check_status')
-            arc_comment.delete()
-        adult_record.health_check_status = 'Done'
-        adult_record.save()
-
-        # Set task to Done if all household members have recompleted their health check
-        # and no ARC comments exist for task
-        all_adults = AdultInHome.objects.filter(application_id=application_id)
-        adults_todo = all_adults.filter(health_check_status='To do')
-        adults_flagged = all_adults.filter(health_check_status='Started')
-        if adults_flagged.count() == 0 and adults_todo.count() == 0:
-            if ArcComments.objects.filter(table_pk=application_id, table_name='ADULT_IN_HOME').count() == 0:
-                if ArcComments.objects.filter(table_pk=application_id, table_name='CHILD_IN_HOME').count() == 0:
-                    application.people_in_home_status = 'COMPLETED'
-                    application.save()
-
-        for adult in all_adults:
-            if adult.health_check_status == 'To do':
-                cookie_key = CustomAuthenticationHandler.get_cookie_identifier()
-                request.COOKIES[cookie_key] = None
-
-                CustomAuthenticationHandler.destroy_session(response)
-                return response
-
-        dbs_qset = AdultInHome.objects.filter(application_id=application_id, capita=False, on_update=True)
-        crc_qset = AdultInHome.objects.filter(application_id=application_id, lived_abroad=True)
-
-        email = user_details.email
-        link = str(settings.PUBLIC_APPLICATION_URL) + '/validate/' + create_account_magic_link(user_details)
-        personalisation = {"link": link,
-                           "firstName": firstname,
-                           "ApplicantName": firstname
-                           }
-
-        if len(dbs_qset) > 0 and len(crc_qset) == 0:
-            dbs_names_string = qset_to_formatted_string(dbs_qset)
-            personalisation["dbs_names"] = dbs_names_string
-            template_id = '9aa3a240-0a00-44bc-ac49-88125eb7c749'
+        if adult_record.health_check_status == 'To do' or adult_record.health_check_status == 'Started':
+            template_id = '8f5713f5-4437-479e-9fcc-262d0306f58c'
+            email = user_details.email
+            link = str(settings.PUBLIC_APPLICATION_URL) + '/validate/' + create_account_magic_link(user_details)
+            personalisation = {"link": link,
+                               "firstName": firstname,
+                               "Household Member Name": adult_name}
             r = send_email(email, personalisation, template_id)
             print(link)
-            print('Just DBS')
+            # Delete ARC comment if it exists after recompleting the household member health check
+            if ArcComments.objects.filter(table_pk=application_id, field_name='health_check_status').count() > 0:
+                arc_comment = ArcComments.objects.get(table_pk=application_id, field_name='health_check_status')
+                arc_comment.delete()
+            adult_record.health_check_status = 'Done'
+            adult_record.save()
 
-        elif len(crc_qset) > 0 and len(dbs_qset) == 0:
-            crc_names_string = qset_to_formatted_string(crc_qset)
-            personalisation["crc_names"] = crc_names_string
-            template_id = '07438eef-d88b-48fe-9812-2bc9e09dbae6'
-            r = send_email(email, personalisation, template_id)
-            print(link)
-            print('Just CRC')
+            # Set task to Done if all household members have recompleted their health check
+            # and no ARC comments exist for task
+            all_adults = AdultInHome.objects.filter(application_id=application_id)
+            adults_todo = all_adults.filter(health_check_status='To do')
+            adults_flagged = all_adults.filter(health_check_status='Started')
+            if adults_flagged.count() == 0 and adults_todo.count() == 0:
+                if ArcComments.objects.filter(table_pk=application_id, table_name='ADULT_IN_HOME').count() == 0:
+                    if ArcComments.objects.filter(table_pk=application_id, table_name='CHILD_IN_HOME').count() == 0:
+                        application.people_in_home_status = 'COMPLETED'
+                        application.save()
 
-        elif len(dbs_qset) > 0 and len(crc_qset) > 0:
-            dbs_names_string = qset_to_formatted_string(dbs_qset)
-            crc_names_string = qset_to_formatted_string(crc_qset)
-            personalisation["dbs_names"] = dbs_names_string
-            personalisation["crc_names"] = crc_names_string
-            template_id = '5d5db808-2c83-41f6-adba-eda1b24c5714'
-            r = send_email(email, personalisation, template_id)
-            print(link)
-            print('DBS and CRC')
+            for adult in all_adults:
+                if adult.health_check_status == 'To do':
+                    cookie_key = CustomAuthenticationHandler.get_cookie_identifier()
+                    request.COOKIES[cookie_key] = None
 
-        else:
-            template_id = '0acc42fa-9ba0-4c5e-8171-e49c08c22b67'
-            r = send_email(email, personalisation, template_id)
-            print(link)
-            print('None')
+                    CustomAuthenticationHandler.destroy_session(response)
+                    return response
 
-        update(application_id, 'people_in_home_status', 'COMPLETED')
+            dbs_qset = AdultInHome.objects.filter(application_id=application_id, capita=False, on_update=True)
+            crc_qset = AdultInHome.objects.filter(application_id=application_id, lived_abroad=True)
+
+            email = user_details.email
+            link = str(settings.PUBLIC_APPLICATION_URL) + '/validate/' + create_account_magic_link(user_details)
+            personalisation = {"link": link,
+                               "firstName": firstname,
+                               "ApplicantName": firstnameand
+                               }
+
+            if len(dbs_qset) > 0 and len(crc_qset) == 0:
+                dbs_names_string = qset_to_formatted_string(dbs_qset)
+                personalisation["dbs_names"] = dbs_names_string
+                template_id = '9aa3a240-0a00-44bc-ac49-88125eb7c749'
+                r = send_email(email, personalisation, template_id)
+                print(link)
+                print('Just DBS')
+
+            elif len(crc_qset) > 0 and len(dbs_qset) == 0:
+                crc_names_string = qset_to_formatted_string(crc_qset)
+                personalisation["crc_names"] = crc_names_string
+                template_id = '07438eef-d88b-48fe-9812-2bc9e09dbae6'
+                r = send_email(email, personalisation, template_id)
+                print(link)
+                print('Just CRC')
+
+            elif len(dbs_qset) > 0 and len(crc_qset) > 0:
+                dbs_names_string = qset_to_formatted_string(dbs_qset)
+                crc_names_string = qset_to_formatted_string(crc_qset)
+                personalisation["dbs_names"] = dbs_names_string
+                personalisation["crc_names"] = crc_names_string
+                template_id = '5d5db808-2c83-41f6-adba-eda1b24c5714'
+                r = send_email(email, personalisation, template_id)
+                print(link)
+                print('DBS and CRC')
+
+            else:
+                template_id = '0acc42fa-9ba0-4c5e-8171-e49c08c22b67'
+                r = send_email(email, personalisation, template_id)
+                print(link)
+                print('None')
+
+            update(application_id, 'people_in_home_status', 'COMPLETED')
 
         adult_record.validated = True
         adult_record.save()
