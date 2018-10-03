76742742a1dc65b8872b0c9211055ee39f322828
Thomas-95is <thomas.stokes@informed.com>
2018-10-03 10:30:27 +0100

    WIP: test_people_in_the_home.py: Plan tests.

--- application/tests/tests_application/test_people_in_the_home.py
+++ application/tests/tests_application/test_people_in_the_home.py
@@ -7,4 +7,8 @@ from django.test import modify_settings, TestCase
         ]
     })
 class PeopleInTheHomeTestSuite(TestCase):
-    pass
+    def test_can_render_guidance_page(self):
+        pass
+
+    def test_post_request_to_guidance_page_redirects_to_adult_page(self):
+        pass
