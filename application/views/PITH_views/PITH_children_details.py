34c3869d31a5a5b85365fe8ab01ede66b1dd1f83
Rica Duchateau <rica.duchateau@informed.com>
2018-10-03 11:10:19 +0100

    Add logging messages for People in the home task

--- application/views/PITH_views/PITH_children_details.py
+++ application/views/PITH_views/PITH_children_details.py
@@ -1,3 +1,5 @@
+import logging
+
 from datetime import date
 
 from django.http import HttpResponseRedirect
@@ -14,12 +16,17 @@ from application.business_logic import (
 from application.forms import OtherPeopleChildrenDetailsForm
 from application.models import Application, ApplicantHomeAddress, AdultInHome
 
+# Initiate logging
+log = logging.getLogger('')
+
 
 class PITHChildrenDetailsView(View):
     """
     Class containing the methods responsible for handling requests to the 'Children-In-The-Home-Details' page.
     """
+
     def get(self, request):
+
         application_id_local = request.GET["id"]
         application = Application.objects.get(pk=application_id_local)
 
@@ -28,21 +35,32 @@ class PITHChildrenDetailsView(View):
         remove_button = True
 
         if number_of_children == 0:  # If there are no children in the database
-            number_of_children = 1   # Set the number of children to 1 to initialise one instance of the form
+
+            number_of_children = 1  # Set the number of children to 1 to initialise one instance of the form
+            log.debug('Number of children set to 1 to initialise one instance of the form')
 
         if number_of_children == 1:
-            remove_button = False    # Disable the remove person button
+            remove_button = False  # Disable the remove person button
+            log.debug('Remove button disabled')
 
         remove_child_in_home(application_id_local, remove_person)
+        log.debug('Child ' + str(remove_person) + ' removed')
         rearrange_children_in_home(number_of_children, application_id_local)
+        log.debug('Children rearranged')
 
-        form_list = [OtherPeopleChildrenDetailsForm(id=application_id_local, child=i, prefix=i) for i in range(1, number_of_children + 1)]
+        form_list = [OtherPeopleChildrenDetailsForm(id=application_id_local, child=i, prefix=i) for i in
+                     range(1, number_of_children + 1)]
+        log.debug('List of children generated')
 
         if application.application_status == 'FURTHER_INFORMATION':
+
             for index, form in enumerate(form_list):
+
                 form.error_summary_template_name = 'returned-error-summary.html'
                 form.error_summary_title = "There was a problem with Child {0}'s details".format(str(index + 1))
                 form.check_flag()
+
+            log.debug('Error summary set up')
 
         variables = {
             'form_list': form_list,
@@ -57,6 +75,7 @@ class PITHChildrenDetailsView(View):
         return render(request, 'other-people-children-details.html', variables)
 
     def post(self, request):
+
         current_date = timezone.now()
         application_id_local = request.POST["id"]
         application = Application.objects.get(pk=application_id_local)
@@ -65,44 +84,65 @@ class PITHChildrenDetailsView(View):
         remove_button = True
 
         if number_of_children == 0:  # If there are no children in the database
-            number_of_children = 1   # Set the number of children to 1 to initialise one instance of the form
+
+            number_of_children = 1  # Set the number of children to 1 to initialise one instance of the form
+            log.debug('Number of children set to 1 to initialise one instance of the form')
 
         if number_of_children == 1:
-            remove_button = False  # Disable the remove person button
 
-        form_list   = []
-        forms_valid = True           # Bool indicating whether or not all the forms are valid
+            remove_button = False  # Disable the remove person button
+            log.debug('Remove button disabled')
+
+        form_list = []
+        forms_valid = True  # Bool indicating whether or not all the forms are valid
         children_turning_16 = False  # Bool indicating whether or not all any children are turning 16
 
         for i in range(1, int(number_of_children) + 1):
+
             form = OtherPeopleChildrenDetailsForm(request.POST, id=application_id_local, child=i, prefix=i)
             form.remove_flag()
             form.error_summary_title = 'There was a problem with Child {0}\'s details'.format(str(i))
             form_list.append(form)
+            log.debug('Form initialised for child ' + str(i))
 
             if application.application_status == 'FURTHER_INFORMATION':
+
                 form.error_summary_template_name = 'returned-error-summary.html'
+                log.debug('Returned error summary template set up')
 
             if form.is_valid():
+
                 child_record = other_people_children_details_logic(application_id_local, form, i)
                 child_record.save()
+                log.debug('Child ' + str(i) + ' record saved to database')
                 reset_declaration(application)
+                log.debug('Declaration status reset')
 
                 # Calculate child's age
                 birth_day, birth_month, birth_year = form.cleaned_data.get('date_of_birth')
                 applicant_dob = date(birth_year, birth_month, birth_day)
                 today = date.today()
 
-                age = today.year - applicant_dob.year - ((today.month, today.day) < (applicant_dob.month, applicant_dob.day))
+                age = today.year - applicant_dob.year - (
+                            (today.month, today.day) < (applicant_dob.month, applicant_dob.day))
+                log.debug('Child ' + str(i) + 'age calculated:' + str(age))
+
                 if 15 <= age < 16:
+
                     children_turning_16 = True
+                    log.debug('Child is approaching 16')
 
             else:
+
                 forms_valid = False
+                log.debug('Form for child ' + str(i) + ' invalid')
 
         if 'submit' in request.POST:
             # If all forms are valid
             if forms_valid:
+
+                log.debug('All forms valid')
+
                 variables = {
                     'application_id': application_id_local,
                     'people_in_home_status': application.people_in_home_status,
@@ -111,11 +151,16 @@ class PITHChildrenDetailsView(View):
                 success_url = self.get_success_url(children_turning_16, application)
                 application.date_updated = current_date
                 application.save()
+                log.debug('Update date updated for application: ' + application_id_local)
                 reset_declaration(application)
+                log.debug('Declaration status reset')
                 return HttpResponseRedirect(reverse(success_url) + '?id=' + application_id_local, variables)
 
             # If there is an invalid form
             else:
+
+                log.debug('Forms invalid')
+
                 variables = {
                     'form_list': form_list,
                     'application_id': application_id_local,
@@ -125,11 +170,16 @@ class PITHChildrenDetailsView(View):
                     'remove_button': remove_button,
                     'people_in_home_status': application.people_in_home_status
                 }
+
                 return render(request, 'other-people-children-details.html', variables)
 
         if 'add_child' in request.POST:
+
             # If all forms are valid
             if forms_valid:
+
+                log.debug('All forms valid')
+
                 variables = {
                     'application_id': application_id_local,
                     'people_in_home_status': application.people_in_home_status
@@ -137,6 +187,8 @@ class PITHChildrenDetailsView(View):
 
                 add_child = int(number_of_children) + 1
                 add_child_string = str(add_child)
+
+                log.debug('Generate URL to add child')
 
                 # Redirect to self.get(), it seems.
                 return HttpResponseRedirect(reverse('PITH-Children-Details-View') + \
@@ -147,6 +199,9 @@ class PITHChildrenDetailsView(View):
 
             # If there is an invalid form
             else:
+
+                log.debug('Forms invalid')
+
                 variables = {
                     'form_list': form_list,
                     'application_id': application_id_local,
@@ -156,8 +211,8 @@ class PITHChildrenDetailsView(View):
                     'remove_button': remove_button,
                     'people_in_home_status': application.people_in_home_status
                 }
-                return render(request, 'other-people-children-details.html', variables)
 
+                return render(request, 'other-people-children-details.html', variables)
 
     def get_success_url(self, children_turning_16, application):
         """
@@ -170,20 +225,39 @@ class PITHChildrenDetailsView(View):
         adults = AdultInHome.objects.filter(application_id=application.pk)
 
         if children_turning_16:
+
+            log.debug('There are children turning 16')
             application.children_turning_16 = True
             success_url = 'PITH-Approaching-16-View'
+
         else:
+
+            log.debug('There are no children turning 16')
             application.children_turning_16 = False
 
             home_address = ApplicantHomeAddress.objects.get(application_id=application.pk, current_address=True)
             childcare_address = ApplicantHomeAddress.objects.get(application_id=application.pk, childcare_address=True)
 
             if home_address == childcare_address:
+
+                log.debug('Applicant cares in own home')
+
                 success_url = 'PITH-Own-Children-Check-View'
+
             else:
+
+                log.debug('Applicant does not care in own home')
+
                 if len(adults) != 0 and any(not adult.capita and not adult.on_update for adult in adults):
+
+                    log.debug('There are adults without a DBS check')
+
                     success_url = 'Task-List-View'
+
                 else:
+
+                    log.debug('There are no adults or all adults have a DBS check')
+
                     success_url = 'PITH-Summary-View'
 
         application.save()
