2b1393bc6546031f225b37115fcc397c25e11f34
proctorj <james.proctor@informed.com>
2018-10-03 10:12:18 +0100

    Removed 'publish my details' page and assoicated files, tests

--- application/tests/tests_views/test_declaration.py
+++ application/tests/tests_views/test_declaration.py
@@ -2,7 +2,7 @@ import random
 
 from django.test import modify_settings
 
-from application.views import publishing_your_details, declaration_intro
+from application.views import declaration_intro
 from .view_parent import *
 import uuid
 
@@ -45,52 +45,48 @@ class DeclarationTest(ViewsTest):
         except:
             self.assertEqual(0, 0)
 
-    def test_url_resolves_to_page_publish_details(self):
-        found = resolve(settings.URL_PREFIX + '/publishing-your-details/')
-        self.assertEqual(found.func, publishing_your_details)
-
     @modify_settings(MIDDLEWARE={
         'remove': [
              'application.middleware.CustomAuthenticationHandler',
         ]
     })
-    def test_can_submit_consent_to_publish_details(self):
-        """
-        Assert that whatever is posted to the publish details page gets inverted in the database.
-        """
-        app_id = uuid.uuid4()
-        models.Application.objects.create(
-            application_id=app_id,
-            application_type='CHILDMINDER',
-            application_status='DRAFTING',
-            cygnum_urn='',
-            login_details_status='NOT_STARTED',
-            personal_details_status='COMPLETED',
-            childcare_type_status='COMPLETED',
-            first_aid_training_status='COMPLETED',
-            childcare_training_status='COMPLETED',
-            criminal_record_check_status='COMPLETED',
-            health_status='COMPLETED',
-            references_status='COMPLETED',
-            people_in_home_status='COMPLETED',
-            declarations_status='NOT_STARTED',
-            date_created=datetime.datetime.today(),
-            date_updated=datetime.datetime.today(),
-            date_accepted=None,
-        )
-        c = Client()
-        publish_details = bool(random.getrandbits(1))
-
-        try:
-            response = c.post(settings.URL_PREFIX + '/publishing-your-details/?id=' + str(app_id),
-                              {'publish_details': publish_details,
-                               'id': str(app_id)})
-
-            application = models.Application.objects.get(application_id=app_id)
-            self.assertEqual(application.publish_details, not publish_details)
-            self.assertEqual(response.status_code, 302)
-        except Exception as e:
-            self.fail(e)
+    # #def test_can_submit_consent_to_publish_details(self):
+    #     """
+    #     Assert that whatever is posted to the publish details page gets inverted in the database.
+    #     """
+    #     app_id = uuid.uuid4()
+    #     models.Application.objects.create(
+    #         application_id=app_id,
+    #         application_type='CHILDMINDER',
+    #         application_status='DRAFTING',
+    #         cygnum_urn='',
+    #         login_details_status='NOT_STARTED',
+    #         personal_details_status='COMPLETED',
+    #         childcare_type_status='COMPLETED',
+    #         first_aid_training_status='COMPLETED',
+    #         childcare_training_status='COMPLETED',
+    #         criminal_record_check_status='COMPLETED',
+    #         health_status='COMPLETED',
+    #         references_status='COMPLETED',
+    #         people_in_home_status='COMPLETED',
+    #         declarations_status='NOT_STARTED',
+    #         date_created=datetime.datetime.today(),
+    #         date_updated=datetime.datetime.today(),
+    #         date_accepted=None,
+    #     )
+    #     c = Client()
+    #     publish_details = bool(random.getrandbits(1))
+    #
+    #     try:
+    #         response = c.post(settings.URL_PREFIX + '/publishing-your-details/?id=' + str(app_id),
+    #                           {'publish_details': publish_details,
+    #                            'id': str(app_id)})
+    #
+    #         application = models.Application.objects.get(application_id=app_id)
+    #         self.assertEqual(application.publish_details, not publish_details)
+    #         self.assertEqual(response.status_code, 302)
+    #     except Exception as e:
+    #         self.fail(e)
 
     def test_status_does_not_change_to_in_progress_when_returning_to_task_list(self):
         test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
