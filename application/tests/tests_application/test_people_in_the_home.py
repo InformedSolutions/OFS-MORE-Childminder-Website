be4a0c48cf3696c15dd54ebf2aaad9e42bedc4b9
Thomas-95is <thomas.stokes@informed.com>
2018-10-03 09:58:25 +0100

    test_people_in_the_home.py: Setup new skeleton for tests.

--- application/tests/tests_application/test_people_in_the_home.py
+++ application/tests/tests_application/test_people_in_the_home.py
@@ -1,29 +1,10 @@
-from unittest import mock
-
-from django.test import TestCase, tag
-from django.urls import reverse
-
-from application import models
-from application.utils import build_url
-
-from .base import ApplicationTestBase
+from django.test import modify_settings, TestCase
 
 
-def parameterise_by_applicant_type(test_func):
-    """
-    Decorator to run a test function for two cases: one in which the applicant is applying for eyfs, and one for which
-    they are applying only for the childcare register.
-    :param test_func: Test function to be decorated.
-    :return: decorated test function.
-    """
-
-    def decorated_test(test_suite):
-        for test_type_ in [test_suite.create_eyfs_applicant, test_suite.create_childcare_register_applicant]:
-            test_type_()
-            test_func(test_suite)
-
-    return decorated_test
-
-
+@modify_settings(MIDDLEWARE={
+        'remove': [
+            'application.middleware.CustomAuthenticationHandler'
+        ]
+    })
 class PeopleInTheHomeTestSuite(TestCase):
     pass
