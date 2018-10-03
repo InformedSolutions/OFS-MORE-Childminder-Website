3461b720cc66351a4c36b011717d2249e9e44707
Thomas-95is <thomas.stokes@informed.com>
2018-10-03 13:44:39 +0100

    WIP: test_people_in_the_home.py: Add more planned tests.

--- application/tests/tests_application/test_people_in_the_home.py
+++ application/tests/tests_application/test_people_in_the_home.py
@@ -10,5 +10,59 @@ class PeopleInTheHomeTestSuite(TestCase):
     def test_can_render_guidance_page(self):
         pass
 
-    def test_post_request_to_guidance_page_redirects_to_adult_page(self):
+    def test_post_request_to_guidance_page_redirects_to_adults_in_home_page(self):
+        pass
+
+    def test_post_request_to_guidance_sets_task_status_to_started(self):
+        pass
+
+    def test_yes_to_adults_in_home_redirects_to_details_page(self):
+        pass
+
+    def test_no_to_adults_in_home_redirects_to_children_in_home_page(self):
+        pass
+
+    def test_can_render_adult_details_page(self):
+        pass
+
+    def test_can_add_one_adult(self):
+        pass
+
+    def test_post_request_to_add_another_adult_adds_extra_form(self):
+        pass
+
+    def test_can_add_two_adults(self):
+        pass
+
+    def test_post_request_to_adult_details_page_redirects_to_lived_abroad_page(self):
+        pass
+
+    def test_can_render_lived_abroad_page(self):
+        pass
+
+    def test_lived_abroad_page_renders_with_one_form_per_adult(self):
+        pass
+
+    def test_no_adults_lived_abroad_redirects_to_dbs_check_if_childcare_register_only_applicant(self):
+        pass
+
+    def test_no_adults_lived_abroad_redirects_to_military_base_page_if_not_childcare_register_only_applicant(self):
+        pass
+
+    def test_any_adults_lived_abroad_redirects_to_abroad_criminal_record_checks_page(self):
+        pass
+
+    def test_can_render_abroad_criminal_record_checks_page(self):
+        pass
+
+    def test_lived_abroad_page_renders_with_all_adults_who_lived_abroad(self):
+        pass
+
+    def test_post_request_to_lived_abroad_page_redirects_to_dbs_check_if_childcare_register_only_applicant(self):
+        pass
+
+    def test_post_request_to_lived_abroad_page_redirects_to_military_base_page_if_not_childcare_register_only_applicant(self):
+        pass
+
+    def test_can_render_military_base_page(self):
         pass
