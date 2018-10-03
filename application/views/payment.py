272750dd31b1b7db15582d274e5abd6137e962a7
proctorj <james.proctor@informed.com>
2018-10-03 13:54:09 +0100

    Updated email template ID's to add HDB posting

--- application/views/payment.py
+++ application/views/payment.py
@@ -72,19 +72,19 @@ def get_template(crc, app_id, application, cost):
                        'cost': cost}
 
     if (capita and not cautions_convictions) and lived_abroad:
-        send_payment_email(email, personalisation, 'ac595e14-1245-43e0-975d-139c8bdf98f9', application)
+        send_payment_email(email, personalisation, '36720ba3-165e-40cd-a6d2-320daa9d6e4a', application)
         return 'payment-confirmation-lived-abroad.html'
 
     elif (not capita or cautions_convictions) and lived_abroad:
-        send_payment_email(email, personalisation, 'ae74eec5-edbe-4b27-b4eb-992ba607d94e', application)
+        send_payment_email(email, personalisation, 'c82b8ffd-f67c-4019-a724-d57ab559f08e', application)
         return 'payment-confirmation-health-dbs.html'
 
     elif (not capita or cautions_convictions) and not lived_abroad:
-        send_payment_email(email, personalisation, '49a9e468-4517-4437-9db9-24b8d913d44e', application)
+        send_payment_email(email, personalisation, '02c01f75-1f9d-428f-a862-4effac03ebd3', application)
         return 'payment-confirmation-dbs-only.html'
 
     elif not lived_abroad and not cautions_convictions:
-        send_payment_email(email, personalisation, '275bac26-d625-4dbd-8f91-a0cc32c700d1', application)
+        send_payment_email(email, personalisation, '8ca4eb7c-f4c9-417a-85e6-f4c10672f41a', application)
         return 'payment-confirmation-no-documents.html'
 
     else:
