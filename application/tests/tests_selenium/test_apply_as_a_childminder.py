cf99791d3f923ff7a3b36a986cc4afb97fef0a3c
Rica Duchateau <rica.duchateau@informed.com>
2018-10-18 17:25:28 +0100

    Add publish your details step back into Selenium tests

--- application/tests/tests_selenium/test_apply_as_a_childminder.py
+++ application/tests/tests_selenium/test_apply_as_a_childminder.py
@@ -656,6 +656,7 @@ class ApplyAsAChildminder(LiveServerTestCase):
 
         self.selenium_task_executor.complete_review()
         self.selenium_task_executor.complete_declaration()
+        self.selenium_task_executor.complete_publish_your_details()
 
         # Card number must be 5454... due to this being a Worldpay API test value
         test_cvc = ''.join(str(random.randint(0, 9)) for _ in range(3))
@@ -842,6 +843,7 @@ class ApplyAsAChildminder(LiveServerTestCase):
 
         self.selenium_task_executor.complete_review()
         self.selenium_task_executor.complete_declaration()
+        self.selenium_task_executor.complete_publish_your_details()
 
         # Card number must be 5454... due to this being a Worldpay API test value
         test_cvc = ''.join(str(random.randint(0, 9)) for _ in range(3))
