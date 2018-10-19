62310a36a7fb703ba70eb02d3bae02d99a2f9568
Rica Duchateau <rica.duchateau@informed.com>
2018-10-19 11:26:16 +0100

    Fix indentation issues

--- application/tests/tests_views/test_declaration.py
+++ application/tests/tests_views/test_declaration.py
@@ -91,43 +91,42 @@ class DeclarationTest(ViewsTest):
         except Exception as e:
             self.fail(e)
 
+    def test_status_does_not_change_to_in_progress_when_returning_to_task_list(self):
+        test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
+        test_login_id = '004551ca-21fa-4dbe-9095-0384e73b3cbe'
+        application = models.Application.objects.create(
+            application_id=(UUID(test_application_id)),
+            application_type='CHILDMINDER',
+            application_status='DRAFTING',
+            cygnum_urn='',
+            login_details_status='NOT_STARTED',
+            personal_details_status='COMPLETED',
+            childcare_type_status='COMPLETED',
+            first_aid_training_status='COMPLETED',
+            childcare_training_status='COMPLETED',
+            criminal_record_check_status='COMPLETED',
+            health_status='COMPLETED',
+            references_status='COMPLETED',
+            people_in_home_status='COMPLETED',
+            declarations_status='NOT_STARTED',
+            date_created=datetime.datetime.today(),
+            date_updated=datetime.datetime.today(),
+            date_accepted=None,
+        )
+        user = models.UserDetails.objects.create(
+            login_id=(UUID(test_login_id)),
+            application_id=application,
+            email='',
+            mobile_number='',
+            add_phone_number='',
+            email_expiry_date=None,
+            sms_expiry_date=None,
+            magic_link_email='',
+            magic_link_sms=''
+        )
+        assert (models.Application.objects.get(pk=test_application_id).declarations_status != 'COMPLETED')
 
-def test_status_does_not_change_to_in_progress_when_returning_to_task_list(self):
-    test_application_id = 'f8c42666-1367-4878-92e2-1cee6ebcb48c'
-    test_login_id = '004551ca-21fa-4dbe-9095-0384e73b3cbe'
-    application = models.Application.objects.create(
-        application_id=(UUID(test_application_id)),
-        application_type='CHILDMINDER',
-        application_status='DRAFTING',
-        cygnum_urn='',
-        login_details_status='NOT_STARTED',
-        personal_details_status='COMPLETED',
-        childcare_type_status='COMPLETED',
-        first_aid_training_status='COMPLETED',
-        childcare_training_status='COMPLETED',
-        criminal_record_check_status='COMPLETED',
-        health_status='COMPLETED',
-        references_status='COMPLETED',
-        people_in_home_status='COMPLETED',
-        declarations_status='NOT_STARTED',
-        date_created=datetime.datetime.today(),
-        date_updated=datetime.datetime.today(),
-        date_accepted=None,
-    )
-    user = models.UserDetails.objects.create(
-        login_id=(UUID(test_login_id)),
-        application_id=application,
-        email='',
-        mobile_number='',
-        add_phone_number='',
-        email_expiry_date=None,
-        sms_expiry_date=None,
-        magic_link_email='',
-        magic_link_sms=''
-    )
-    assert (models.Application.objects.get(pk=test_application_id).declarations_status != 'COMPLETED')
 
-
-def delete(self):
-    models.Application.objects.get(pk='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
-    models.UserDetails.objects.get(login_id='004551ca-21fa-4dbe-9095-0384e73b3cbe').delete()
+    def delete(self):
+        models.Application.objects.get(pk='f8c42666-1367-4878-92e2-1cee6ebcb48c').delete()
+        models.UserDetails.objects.get(login_id='004551ca-21fa-4dbe-9095-0384e73b3cbe').delete()
