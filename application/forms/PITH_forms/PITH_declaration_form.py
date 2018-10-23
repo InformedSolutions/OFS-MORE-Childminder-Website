161ca6dafb2bd9d99a2a1d227e4b524adf2e824a
Sam Jones <sam.jones@informed.com>
2018-10-23 12:10:52 +0100

    Changed content for both the error summary title and the confirmation message

--- application/forms/PITH_forms/PITH_declaration_form.py
+++ application/forms/PITH_forms/PITH_declaration_form.py
@@ -8,11 +8,12 @@ class PITHDeclarationForm(ChildminderForms):
     """
     field_label_classes = 'form-label-bold'
     error_summary_template_name = 'standard-error-summary.html'
+    error_summary_title = 'There was a problem'
     auto_replace_widgets = True
 
     declaration_confirmation = forms.BooleanField(label='I confirm', required=True,
                                                   error_messages={
-                                                      'required': 'You must confirm everything on this page to continue'})
+                                                      'required': 'You must confirm that the information you have given is correct'})
 
     def clean_declaration_confirmation(self):
         """
@@ -21,5 +22,5 @@ class PITHDeclarationForm(ChildminderForms):
         """
         declaration_confirmation = self.cleaned_data['declaration_confirmation']
         if not declaration_confirmation:
-            raise forms.ValidationError('You must confirm everything on this page to continue')
+            raise forms.ValidationError('You must confirm that the information you have given is correct')
         return declaration_confirmation
