70a7d1ef10069e3375c6ea68ffffda79483b5f71
joness <sam.jones@informed.com>
2018-10-24 12:11:36 +0100

    Added ARC search visibility to the children details field of the health check

--- application/views/other_people_health_check/local_authorities.py
+++ application/views/other_people_health_check/local_authorities.py
@@ -2,7 +2,7 @@ import logging
 
 from django.core.exceptions import ObjectDoesNotExist
 
-from application.forms.other_person_health_check.local_authorites import LocalAuthorities
+from application.forms.other_person_health_check.local_authorities import LocalAuthorities
 from application.models import AdultInHome, HealthCheckCurrent
 from application.views.other_people_health_check.BaseViews import BaseFormView
 
@@ -15,7 +15,7 @@ class LocalAuthorities(BaseFormView):
     """
     template_name = 'other_people_health_check/local_authorities.html'
     form_class = LocalAuthorities
-    success_url = 'Health-Check-Local-Authorities'
+    success_url = 'Health-Check-Declaration'
 
     def get_initial(self):
         """
@@ -27,9 +27,9 @@ class LocalAuthorities(BaseFormView):
         try:
             person_id = self.request.GET['person_id']
             person_record = AdultInHome.objects.get(pk=person_id)
-            current_illness_record = HealthCheckCurrent.objects.get(person_id=person_id)
+            #current_illness_record = HealthCheckCurrent.objects.get(person_id=person_id)
             initial['known_to_council'] = person_record.current_treatment
-            initial['children_details'] = current_illness_record.description
+            initial['children_details'] = person_record.children_details
         # If there has yet to be an entry for the model associated with the form, then no population necessary
         except ObjectDoesNotExist:
             pass
@@ -43,36 +43,42 @@ class LocalAuthorities(BaseFormView):
         :return: an http redirect to the success url
         """
 
-        clean = form.cleaned_data
+        cleaned_data = form.cleaned_data
         person_id = self.request.GET['person_id']
-        new_fields = {
-            'person_id' :AdultInHome.objects.get(pk=person_id),
-            'description': clean['illness_details']
-        }
+        # new_fields = {
+        #     'person_id' :AdultInHome.objects.get(pk=person_id),
+        #     'description': clean['children_details']
+        # }
 
         person_record = AdultInHome.objects.get(pk=person_id)
 
         # If they've said no to being currently treated, if any records exist with their id, delete them
-        logger.debug('Clearing current illness details for person id: ' + str(person_id) + 'as response has been renewed')
-        HealthCheckCurrent.objects.filter(person_id=person_record).delete()
+        # logger.debug('Clearing childrens details for person id: ' + str(person_id) + 'as response has been renewed')
+        # HealthCheckCurrent.objects.filter(person_id=person_record).delete()
 
-        if clean['currently_ill'] == 'True':
-            person_record.current_treatment = True
+        if cleaned_data['known_to_council'] == 'True':
+            person_record.known_to_council = True
+            person_record.children_details = cleaned_data['children_details']
         else:
-            person_record.current_treatment = False
+            person_record.known_to_council = False
+            person_record.children_details = ''
 
-        if person_record.current_treatment:
-            # If they say they are currently being treated, create a record saying so, or update an existing record
+        if person_record.known_to_council:
+            # If they say they are currently known to the council, create a record saying so, or update an existing record
             # with the new data they've entered
 
-            logger.debug('Updating current illness details for person id: ' + str(person_id))
+            logger.debug('Updating current childrens details for person id: ' + str(person_id))
 
             # Created is a boolean for verification purposes, not used here but required to call the method
-            new_current_illness, created = HealthCheckCurrent.objects.update_or_create(
-                person_id=person_record,
-                description=clean['illness_details'],
-                defaults=new_fields,
-            )
+            # new_current_illness, created = AdultInHome.objects.update_or_create(
+            #     person_id=person_record,
+            #     description=clean['children_details'],
+            #     defaults=new_fields,
+            # )
+            #
+            # adult = AdultInHome.objects.get(pk=person_id)
+            # adult.known_to_council = 'BLAH'
+            # adult.save()
 
         # As the current treatment boolean in the adult in home table has been updated, save the record
         person_record.save()
