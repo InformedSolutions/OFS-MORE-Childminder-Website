34d2062d9caf66cf286af44178373810659a6ac3
Thomas-95is <thomas.stokes@informed.com>
2018-11-21 16:16:46 +0000

    testcoverage.sh: Remove tests and settings from report.

--- testcoverage.sh
+++ testcoverage.sh
@@ -3,7 +3,7 @@
 . ./dev-env-vars.txt
 rm -r htmlcov 2> /dev/null
 coverage erase
-coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-childminder/manage.py test --settings=childminder.settings.dev --exclude-tag=selenium --no-input
-coverage report -m --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*
-coverage html --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* -d htmlcov
+coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*,*tests/*,*settings/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-childminder/manage.py test --settings=childminder.settings.dev --exclude-tag=selenium --no-input
+coverage report -m --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*,*tests/*,*settings/*
+coverage html --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/*,*tests/*,*settings/* -d htmlcov
 firefox htmlcov/index.html
