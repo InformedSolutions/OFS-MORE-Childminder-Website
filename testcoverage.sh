73338b062ee47326232f5edc5c448fc021a63b5e
Thomas-95is <thomas.stokes@informed.com>
2018-11-21 15:35:08 +0000

    testcoverage.sh: Add env vars to test coverage statistics.

--- testcoverage.sh
+++ testcoverage.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # Script to test unit test coverage for app-childminder and produce html document from report
+. ./dev-env-vars.txt
 rm -r htmlcov 2> /dev/null
 coverage erase
 coverage run --omit=.env/*,*/migrations/*,*/__init__.py,*/models.py,manage.py,/home/vagrant/example-venv/* --branch /vagrant/OFS-MORE-DevOps-Tooling/app-childminder/manage.py test --settings=childminder.settings.dev --exclude-tag=selenium --no-input
