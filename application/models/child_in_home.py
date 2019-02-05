b151f35c4bf953fcef5304a7a9351fb88e1717ea
James Cruddas <james.cruddas@informed.com>
2019-02-05 12:21:58 +0000

    Add child relationship cygnum field to model

--- application/models/child_in_home.py
+++ application/models/child_in_home.py
@@ -7,7 +7,8 @@ class ChildInHome(ChildBase):
     Model for CHILD_IN_HOME table
     """
     relationship = models.CharField(max_length=100, blank=True)
-
+    cygnum_relationship_to_childminder = models.CharField(max_length=100, blank=True)
+    
     @classmethod
     def get_id(cls, app_id):
         return cls.objects.get(application_id=app_id)
