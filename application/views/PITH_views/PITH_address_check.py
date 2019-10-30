c13cee29eda10e6e69b72970b12b229f047e30f4
trichmond-IS <thomas.richmond@informed.com>
2019-10-30 16:46:48 +0000

    wip - copied format of PITH_lived_abroad and DBS_check, still no applicant address shown

--- application/views/PITH_views/PITH_address_check.py
+++ application/views/PITH_views/PITH_address_check.py
@@ -4,38 +4,48 @@ from django.http import HttpResponseRedirect
 
 from application.utils import build_url, get_id
 from application.models import AdultInHome, AdultInHomeAddress, ApplicantHomeAddress, ApplicantPersonalDetails, Application
-from application.views.PITH_views.base_views.PITH_radio_view import PITHRadioView
+from application.views.PITH_views.base_views.PITH_multi_radio_view import PITHMultiRadioView
 from application.forms.PITH_forms.PITH_address_check import PITHAddressDetailsCheckForm
+
 from .. import address_helper, status
 # Initiate logging
 log = logging.getLogger('')
 
 
-class PITHAdultAddressCheckView(PITHRadioView):
+class PITHAdultAddressCheckView(PITHMultiRadioView):
 
     template_name = 'PITH_templates/PITH_address_check.html'
     form_class = PITHAddressDetailsCheckForm
     success_url = ('PITH-Lived-Abroad-View', 'PITH-Address-Details-View')
     PITH_field_name = 'adult_in_home_address'
 
-    def get(self, request, *args, **kwargs):
-
-        application_id = get_id(request)
-        num_adults = int(request.GET('adults'))
-
-        # Rearrange adults to remove empty spaces in adult list.
-        rearrange_adults(num_adults, application_id)
-        log.debug('Adults rearranged')
-
-        return super().get(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-
+    def get_form_kwargs(self, adult=None):
+        """
+        Returns the keyword arguments for instantiating the form.
+        """
         application_id = get_id(self.request)
-        num_adults = int(self.request.GET.get('adults'))
-        PITH_status = get_application(application_id, 'people_in_home_status')
-        num_adults = 1 if num_adults == 0 else num_adults
+        personal_detail_id = ApplicantPersonalDetails.objects.get(
+            application_id=application_id).personal_detail_id
+        applicant_home_address = ApplicantHomeAddress.objects.get(personal_detail_id=personal_detail_id,
+                                                                  current_address=True)
+        street_line1 = applicant_home_address.street_line1
+        street_line2 = applicant_home_address.street_line2
+        town = applicant_home_address.town
+        county = applicant_home_address.county
+        postcode = applicant_home_address.postcode
+        context = {
+            'id': application_id,
+            'street_line1': street_line1,
+            'street_line2': street_line2,
+            'town': town,
+            'county': county,
+            'postcode': postcode,
+            'PITH_field_name': self.PITH_field_name,
+            'adult': adult}
 
+        log.debug('Return keyword arguments to instantiate the form')
+
+        return super().get_form_kwargs(context)
 
     def get_success_url(self, get=None):
         """
@@ -61,12 +71,13 @@ class PITHAdultAddressCheckView(PITHRadioView):
         log.debug('Checking if form is valid')
 
         application_id = get_id(self.request)
+
         adults = AdultInHome.objects.filter(application_id=application_id)
 
         for adult in adults:
-            same_address_bool = self.request.POST.get(self.PITH_field_name+str(adult.pk))
+            PITH_same_address_bool = self.request.POST.get(self.PITH_field_name + str(adult.pk))
 
-            setattr(adult, self.PITH_field_name, same_address_bool)
+            setattr(adult, self.PITH_field_name, PITH_same_address_bool)
             adult.save()
 
         return super().form_valid(form)
@@ -74,7 +85,8 @@ class PITHAdultAddressCheckView(PITHRadioView):
     def get_form_list(self):
 
         application_id = get_id(self.request)
-        adults = AdultInHomeAddress.objects.filter(application_id=application_id)
+
+        adults = AdultInHome.objects.filter(application_id=application_id)
         form_list = [self.form_class(**self.get_form_kwargs(adult=adult)) for adult in adults]
         sorted_form_list = sorted(form_list, key=lambda form: form.adult.adult)
 
@@ -85,41 +97,30 @@ class PITHAdultAddressCheckView(PITHRadioView):
     def get_initial(self):
 
         application_id = get_id(self.request)
-        personal_detail_id = ApplicantPersonalDetails.objects.get(
-            application_id=application_id).personal_detail_id
-        applicant_home_address = ApplicantHomeAddress.objects.get(personal_detail_id=personal_detail_id,
-                                                                  current_address=True)
-        street_line1 = applicant_home_address.street_line1
-        street_line2 = applicant_home_address.street_line2
-        town = applicant_home_address.town
-        county = applicant_home_address.county
-        postcode = applicant_home_address.postcode
 
-        initial = {}
-        adults = AdultInHomeAddress.objects.filter(application_id=application_id)
-        for adult in adults:
-            initial[self.PITH_field_name+str(adult.pk)] = adult.adult_in_home_address
-        initial[self.street_line1] = street_line1
-        initial[self.street_line2] = street_line2
-        initial[self.town] = town
-        initial[self.county] = county
-        initial[self.postcode] = postcode
+        adults = AdultInHome.objects.filter(application_id=application_id)
+
+        initial_context = {self.PITH_field_name + str(adult.pk): adult.PITH_same_address
+                           for adult in adults}
 
         log.debug('Form field data initialised')
-        return initial
+
+        return initial_context
 
     def get_choice_url(self, app_id):
 
-        adults = AdultInHomeAddress.objects.filter(application_id=app_id)
+        adults = AdultInHome.objects.filter(application_id=app_id)
 
-        yes_choice, no_yes_choice, no_no_choice = self.success_url
+        yes_choice, no_choice = self.success_url
 
-        if any(adult.adult_in_home_address for adult in adults):
+        if any(adult.PITH_same_address for adult in adults):
 
-            log.debug('Adults at same address')
+            log.debug('Adult lives at applicant\'s address')
 
             return yes_choice
 
         else:
 
-            log.debug('Adults not at same address')
+            log.debug('Adults live at different address')
+
+            return no_choice
