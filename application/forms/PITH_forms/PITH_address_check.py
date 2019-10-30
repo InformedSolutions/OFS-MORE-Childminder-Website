7b1576a0eda28f4f4f8564814f94efabb5953386
trichmond-IS <thomas.richmond@informed.com>
2019-10-30 09:08:20 +0000

    wip-add new PITHAddress Table to db, and begun integration

--- application/forms/PITH_forms/PITH_address_check.py
+++ application/forms/PITH_forms/PITH_address_check.py
@@ -4,12 +4,13 @@ from django import forms
 from django.conf import settings
 from govuk_forms.widgets import InlineRadioSelect
 
+#from application.forms.PITH_forms.PITH_base_forms.PITH_radio_form import PITHRadioForm
 from application.forms.childminder import ChildminderForms
 from application.forms_helper import full_stop_stripper
 from application.models import (ApplicantHomeAddress,
                                 ApplicantPersonalDetails)
 
-class PersonalDetailsLocationOfPITH(ChildminderForms):
+class PITHAddressDetailsCheckForm(ChildminderForms):
     """
     GOV.UK form for the Your personal details: location of care page
     """
@@ -21,7 +22,7 @@ class PersonalDetailsLocationOfPITH(ChildminderForms):
         ('True', 'Yes'),
         ('False', 'No')
     )
-    childcare_location = forms.ChoiceField(label='Is this where they live??',
+    adult_in_home_address = forms.ChoiceField(label='Is this where they live??',
                                            choices=options,
                                            widget=InlineRadioSelect, required=True,
                                            error_messages={
@@ -34,7 +35,7 @@ class PersonalDetailsLocationOfPITH(ChildminderForms):
         :param kwargs: keyword arguments passed to the form, e.g. application ID
         """
         self.application_id_local = kwargs.pop('id')
-        super(PersonalDetailsLocationOfPITH, self).__init__(*args, **kwargs)
+        super(PITHAddressDetailsCheckForm, self).__init__(*args, **kwargs)
         full_stop_stripper(self)
         # If information was previously entered, display it on the form
         personal_detail_id = ApplicantPersonalDetails.objects.get(
@@ -42,6 +43,6 @@ class PersonalDetailsLocationOfPITH(ChildminderForms):
         if ApplicantHomeAddress.objects.filter(personal_detail_id=personal_detail_id, current_address=True).count() > 0:
             address = ApplicantHomeAddress.objects.get(
                 personal_detail_id=personal_detail_id, current_address=True)
-            self.fields['childcare_location'].initial = address.childcare_address
-            self.field_list = ['childcare_location']
+            self.fields['adult_in_home_address'].initial = address.adult_in_home_address
+            self.field_list = ['adult_in_home_address']
             self.pk = address.pk
---
+++ application/models/adult_in_home_address.py
@@ -0,0 +1,44 @@
+from uuid import uuid4
+from django.db import models
+from .application import Application
+
+
+class AdultInHomeAddress(models.Model):
+    """
+    Model for ADULT_IN_HOME_ADDRESS table
+    """
+    adult_in_home_address_id = models.UUIDField(primary_key=True, default=uuid4)
+    application_id = models.ForeignKey(Application, on_delete=models.CASCADE, db_column='application_id')
+    adult_in_home_address = models.IntegerField(blank=True)
+    street_line1 = models.CharField(max_length=100, blank=True)
+    street_line2 = models.CharField(max_length=100, blank=True)
+    town = models.CharField(max_length=100, blank=True)
+    county = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
+    postcode = models.CharField(max_length=8, blank=True)
+
+    @property
+    def timelog_fields(self):
+        """
+        Specify which fields to track in this model once application is returned.
+
+        Used for signals only. Check base.py for available signals.
+        This is used for logging fields which gonna be updated by applicant
+        once application status changed to "FURTHER_INFORMATION" on the arc side
+
+        Returns:
+            tuple of fields which needs update tracking when application is returned
+        """
+
+        return (
+            'adult_in_home_address',
+            'street_line1',
+            'street_line2',
+            'town',
+            'county',
+            'country',
+            'postcode',
+        )
+
+    class Meta:
+        db_table = 'ADULT_IN_HOME_ADDRESS'
