95e470fc695e5f6944039b28ad4e35affedd5eb6
trichmond-IS <thomas.richmond@informed.com>
2020-03-10 16:08:52 +0000

    prevent user from returning to list with no childcare address

--- application/views/personal_details.py
+++ application/views/personal_details.py
@@ -524,7 +524,9 @@ def personal_details_location_of_care(request):
                 return HttpResponseRedirect(reverse('Personal-Details-Summary-View') + '?id=' + app_id)
 
             else:
-
+                if not ApplicantHomeAddress.objects.filter(personal_detail_id=personal_detail_id,
+                                                       childcare_address=True).exists():
+                    status.update(app_id, 'personal_details_status', 'IN_PROGRESS')
                 return HttpResponseRedirect(reverse('Personal-Details-Childcare-Address-View') + '?id=' + app_id)
         else:
 
@@ -1122,6 +1124,7 @@ def personal_details_summary(request):
             applicant_childcare_address_record = ApplicantHomeAddress.objects.get(personal_detail_id=personal_detail_id,
                                                                                   childcare_address=True)
         else:
+            status.update(app_id, 'personal_details_status', 'IN_PROGRESS')
             return HttpResponseRedirect(reverse('Personal-Details-Childcare-Address-View') + '?id=' + app_id)
 
 
